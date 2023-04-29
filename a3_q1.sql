01f02a3ae1b4afac471826264180c83a82056ae5
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-04-29 18:16:02 +0800

    Find each parent company have how many subsidiary company
    There are 8 distinct parent companies and the rest of the company have no parent companies
    Using: "GROUP BY"
    	to calculate the numbers of companies under each parent company
    	&how many of them have no parent company(NULL)

--- a3_q1.sql
+++ a3_q1.sql
@@ -3,6 +3,7 @@ USE A3_farm_records;
 
 SHOW TABLES;
 DESCRIBE Company_Info;
+SELECT * FROM Company_Info;
 SELECT COUNT(*) FROM Company_Info;
 SELECT  COUNT(Company_name) FROM Company_Info;
 SELECT  COUNT(DISTINCT Company_name) FROM Company_Info;
@@ -10,6 +11,9 @@ SELECT  COUNT(DISTINCT Parent_company) FROM Company_Info;
 SELECT MIN(LENGTH(Company_name)) AS min_length_com, MAX(LENGTH(Company_name)) AS max_length_com,
 MIN(LENGTH(Parent_company)) AS min_length_pc, MAX(LENGTH(Parent_company)) AS max_length_pc
 FROM Company_Info;
+
+SELECT  DISTINCT Parent_company,COUNT(Company_name) FROM Company_Info
+GROUP BY Parent_company;
 
 DESCRIBE Seed_Info;
 SELECT COUNT(*) FROM Seed_Info;
@@ -25,4 +29,5 @@ SELECT COUNT(*) FROM Trial_Info;
 
 DESCRIBE Employee_Info;
 SELECT COUNT(*) FROM Employee_Info;
+
 
