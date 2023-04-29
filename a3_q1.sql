8da1aff853f801a60f68436150d9b91355d314f0
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-04-29 16:33:39 +0800

    Get the row numbers of each tables
    The row numbers represent that the data inputed in table
    Using: SELECT COUNT(*) FROM table_name

--- a3_q1.sql
+++ a3_q1.sql
@@ -3,9 +3,20 @@ USE A3_farm_records;
 
 SHOW TABLES;
 DESCRIBE Company_Info;
+SELECT COUNT(*) FROM Company_Info;
+
 DESCRIBE Seed_Info;
+SELECT COUNT(*) FROM Seed_Info;
+
 DESCRIBE State_Info;
+SELECT COUNT(*) FROM State_Info;
+
 DESCRIBE Farm_Info;
+SELECT COUNT(*) FROM Farm_Info;
+
 DESCRIBE Trial_Info;
+SELECT COUNT(*) FROM Trial_Info;
+
 DESCRIBE Employee_Info;
+SELECT COUNT(*) FROM Employee_Info;
 
