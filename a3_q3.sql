3bc38d8f9859b62ba61c514291b31387643bc57b
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-04-30 14:25:46 +0800

    Find the irrigation status of each states
    Using:"GROUP BY" State_name and then Irrigation
    	to know under certain state there are how many farms have irrigation

--- a3_q3.sql
+++ a3_q3.sql
@@ -4,9 +4,14 @@ USE A3_farm_records;
 DESCRIBE State_Info;
 DESCRIBE Farm_Info;
 
-SELECT State_name,COUNT(Farm_name)  
+SELECT State_name,COUNT(Farm_name)
 FROM State_Info 
 JOIN Farm_Info ON State_Info.State_ID =Farm_Info.State_id 
 GROUP BY State_name 
-HAVING COUNT(Farm_name)>6
+ORDER BY COUNT(Farm_name)DESC;
+
+SELECT State_name,Irrigation,COUNT(Farm_name)
+FROM State_Info 
+JOIN Farm_Info ON State_Info.State_ID =Farm_Info.State_id 
+GROUP BY State_name,Irrigation 
 ORDER BY COUNT(Farm_name)DESC;
