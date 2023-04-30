3071215d1a892124faa47e603824f032fafc7895
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-04-30 13:35:32 +0800

    Find the no of crops and information of maturity betweeen different parent company
    Using the same method as above replace company_name by parent_comapany
    to observe the differences between each parent company.
    The result show that the number of crops and min, max, mean, variance of maturity of seed between each parent company is similar

--- a3_q2.sql
+++ a3_q2.sql
@@ -31,4 +31,15 @@ ON Company_Info.Company_ID =Seed_Info.Company_id
 GROUP BY Company_name
 ORDER BY COUNT(DISTINCT Crop)DESC LIMIT 5; 
 
+SELECT Parent_company,
+    COUNT(DISTINCT Crop) AS No_crop,
+    ROUND(MIN(Maturity)) AS min_maturity,
+    ROUND(MAX(Maturity)) AS max_maturity,
+    ROUND(AVG(Maturity)) AS mean_maturity,
+    ROUND(VARIANCE(Maturity)) AS var_maturity
+FROM Company_Info  
+JOIN Seed_Info 
+ON Company_Info.Company_ID =Seed_Info.Company_id
+GROUP BY Parent_company
+ORDER BY COUNT(DISTINCT Crop) DESC; 
 
