66e98afa014f3f83a049ab4d786aed63357d16ab
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-04-30 12:26:02 +0800

    Find the number of seeds release by each company
    Using:"JOIN" to join the Company_Info and Seed_Info
    	by their company id
          "GROUP BY" Company_name
    	to  know number of seed released by each company
          "ORDER BY" COUNT(*) DESC  & "LIMIT 1"
    	to sort the numbers of seed  of each company in descending order
    	to know the company who release the most number of seeds

--- a3_q2.sql
+++ a3_q2.sql
@@ -3,3 +3,10 @@ USE A3_farm_records;
 
 DESCRIBE Company_Info;
 DESCRIBE Seed_Info;
+
+SELECT Company_name,COUNT(*) AS No_seed 
+FROM Company_Info  
+JOIN Seed_Info 
+ON Company_Info.Company_ID =Seed_Info.Company_id
+GROUP BY Company_name
+ORDER BY COUNT(*) DESC; 
