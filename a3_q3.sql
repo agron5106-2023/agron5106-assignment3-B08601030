037ec7578f4bd260f771e8b19b9e8577f3d1060f
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-04-30 14:05:18 +0800

    Find the number of farms per state
    Using:"JOIN" join the Farm_Info & State_Info
          "SELECT"&"GROUP BY State_name" to know the number of farms per state
          "ORDER BY"sort it by the numbers of farms in a descending order

--- a3_q3.sql
+++ a3_q3.sql
@@ -3,3 +3,9 @@ USE A3_farm_records;
 
 DESCRIBE State_Info;
 DESCRIBE Farm_Info;
+
+SELECT State_name,COUNT(Farm_name)  
+FROM State_Info 
+JOIN Farm_Info ON State_Info.State_ID =Farm_Info.State_id 
+GROUP BY State_name 
+ORDER BY COUNT(Farm_name)DESC;
