e859439e4e4139384ba6e8c6ebee1cfd2a17eca0
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-01 22:21:35 +0800

    Find the highest disease rating group by crop then state
    The below crops and states have highest avg disease rating(10):
    Barley_h,Iowa
    Soybean Oil,Massachusetts
    Soybean,New Jersey

--- a3_q4.sql
+++ a3_q4.sql
@@ -48,13 +48,14 @@ ORDER BY Avg_Disease_rating DESC;
 
 SELECT 
 	Crop,
+	State_name,
 	COUNT(Farm_name), 
 	AVG(Disease_rating) AS Avg_Disease_rating 
 FROM Trial_Info 
 JOIN Farm_Info ON Trial_Info.Farm_info_id = Farm_Info.Farm_ID
 JOIN State_Info ON State_Info.State_ID = Farm_Info.State_id
 JOIN Seed_Info ON Seed_Info.Seed_ID = Trial_Info.Seed_info_id 
-GROUP BY Crop
+GROUP BY Crop,State_name
 ORDER BY Avg_Disease_rating DESC;
 
 
