8c2c2b72585acfb34285720ee8fd9d1691cbd324
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-01 21:25:44 +0800

    Find the state having highest total yield
    we can also find the state that have highest total yield without certain crop
    California has the highest total yield because it has the most number of farms

--- a3_q4.sql
+++ a3_q4.sql
@@ -7,17 +7,30 @@ DESCRIBE Farm_Info;
 DESCRIBE Seed_Info;
 
 SELECT 
+	State_name,
+	COUNT(Farm_name), 
+	SUM(Yield) AS Total_yield,
+	AVG(Yield) AS Avg_yield
+FROM Trial_Info 
+JOIN Farm_Info ON Trial_Info.Farm_info_id = Farm_Info.Farm_ID
+JOIN State_Info ON State_Info.State_ID = Farm_Info.State_id
+JOIN Seed_Info ON Seed_Info.Seed_ID = Trial_Info.Seed_info_id 
+GROUP BY State_name
+ORDER BY Total_yield DESC;
+
+SELECT 
 	Crop, 
 	State_name,
 	COUNT(Farm_name), 
-	SUM(Yield) AS Total_yield
+	SUM(Yield) AS Total_yield,
+	AVG(Yield) AS Avg_yield 
 FROM Trial_Info 
 JOIN Farm_Info ON Trial_Info.Farm_info_id = Farm_Info.Farm_ID
 JOIN State_Info ON State_Info.State_ID = Farm_Info.State_id
 JOIN Seed_Info ON Seed_Info.Seed_ID = Trial_Info.Seed_info_id 
 WHERE Crop LIKE 'Rice%'
-GROUP BY State_name, Crop
-ORDER BY Total_yield DESC;
+GROUP BY State_name,Crop
+ORDER BY Avg_yield DESC;
 
 
 
