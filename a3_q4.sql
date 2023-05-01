ea856a4d32e2fe13a220bc1e03ef7b5a01e433a4
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-01 21:07:48 +0800

    Find the state that have highest rice yield
    Using:"SUM Yield" & condition under Crop LIKE rice%
    	sort in descending order
    Result:California have the highest total rice yield

--- a3_q4.sql
+++ a3_q4.sql
@@ -3,8 +3,22 @@ USE A3_farm_records;
 
 SHOW TABLES;
 DESCRIBE Trial_Info;
-DESCRIBE Seed_Info;
 DESCRIBE Farm_Info;
+DESCRIBE Seed_Info;
+
+SELECT 
+	Crop, 
+	State_name,
+	COUNT(Farm_name), 
+	SUM(Yield) AS Total_yield
+FROM Trial_Info 
+JOIN Farm_Info ON Trial_Info.Farm_info_id = Farm_Info.Farm_ID
+JOIN State_Info ON State_Info.State_ID = Farm_Info.State_id
+JOIN Seed_Info ON Seed_Info.Seed_ID = Trial_Info.Seed_info_id 
+WHERE Crop LIKE 'Rice%'
+GROUP BY State_name, Crop
+ORDER BY Total_yield DESC;
+
 
 
 
